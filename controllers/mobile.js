2a6b7121217a3a0b05a4f65cf60535d98fc8bdd0
Wanrick <wanrick.pro@gmail.com>
2017-07-10 20:33:22 +0200

    feat(api): Database integration
    Added a schema for Sessions in models folder

--- controllers/mobile.js
+++ controllers/mobile.js
@@ -1,3 +1,4 @@
+var Session     = require('../models/session');
 var debug       = require('debug')('platypus-api:controllers:mobile');
 
 debug('Exporting method: createSession');
@@ -7,7 +8,23 @@ module.exports.createSession = function(req, res, next){
 
   debug('Nickname: ' + nickname + ' Color: ' + user_color);
   //@todo var session_vars = mongoMagic(nickname, user_color): return [session_id, user_id];
-
+  var new_session = Session({
+    session_id  : null,
+    image_path  : "",
+    bill_items  : [{
+      item_id       : null,
+      item_name     : null,
+      item_quantity : null,
+      item_price    : null
+    }],
+    users       : [{
+      u_id          : {type: Number, index: { unique: true }},
+      u_owner       : true,
+      u_nickname    : nickname,
+      u_color       : user_color,
+      u_claimed     : [null]
+    }],
+  });
 
   var response = {
     data: {
---
+++ models/session.js
@@ -0,0 +1,52 @@
+var bcrypt      = require('bcryptjs');
+var debug       = require('debug')('platypus-api:models:session');
+var mongoose    = require('mongoose');
+
+// Typedefs for easier usage.
+var ObjectId    = mongoose.Schema.Types.ObjectId;
+var Schema      = mongoose.Schema;
+
+debug('Defining schema: Session');
+var Session = new Schema({
+  session_id  : {type: Number, index: { unique: true }},
+  image_path  : String,
+  bill_items  : [{
+    item_id       : {type: Number, index: { unique: true }},
+    item_name     : String,
+    item_quantity : Number,
+    item_price    : Number
+  }],
+  users       : [{
+    u_id          : {type: Number, index: { unique: true }},
+    u_owner       : Boolean,
+    u_nickname    : String,
+    u_color       : String,
+    u_claimed     : [{item: Number, quantity: Number}]
+  }],
+});
+
+debug('Adding custom schema method: generateSessionID');
+Session.methods.generateSessionID = function(){
+  var alphabet = "abcdefghijklmnopqrstuvwxyz";
+  var numbers = "0123456789";
+  var session_id_temp = "";
+
+  for (var i = 0; i < 5; i++) {
+    var char_or_int = (Math.floor(Math.random() * 2));
+
+    if (char_or_int === 0) {
+      var alphabet_index = (Math.floor(Math.random() * 26));
+      session_id_temp = session_id_temp + alphabet.charAt(alphabet_index);
+    }
+    else {
+      var numbers_index = (Math.floor(Math.random() * 10));
+      session_id_temp = session_id_temp + numbers.charAt(numbers_index);
+    }
+  }
+
+  debug('Setting session_id_temp = ' + session_id_temp);
+  this.session_id = session_id_temp;
+};
+
+debug('Session model exported');
+module.exports = mongoose.model('Session', Session);
