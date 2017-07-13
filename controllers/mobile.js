c5bd2e9aabf3268cfbdc91a8ec1794dd8fe2f90e
Wanrick <wanrick.pro@gmail.com>
2017-07-13 18:53:57 +0200

    feat(db): Receive image and update database

--- controllers/mobile.js
+++ controllers/mobile.js
@@ -1,12 +1,7 @@
 var Bills     = require('../models/bills');
 var debug     = require('debug')('platypus-api:controllers:mobile');
+var fs        = require('fs');
 var multer    = require('multer');
-var upload    = multer({ dest: '../uploads/' });
-
-// upload.single('avatar'), function (req, res, next) {
-//   // req.file is the `avatar` file
-//   // req.body will hold the text fields, if there were any
-// })
 
 debug('Exporting method: createSession');
 /**
@@ -34,7 +29,7 @@ module.exports.createSession = function(req, res, next){
   bill.save(function(err) {
     if (err) {
       // TODO: Create Error class for db errors
-      return 0;
+      debug(err);
     }
   });
 
@@ -53,14 +48,28 @@ module.exports.createSession = function(req, res, next){
 }
 
 debug("Exporting method sendImage");
+/**
+ * Function call to upload a file to the server. Image is saved in ./uploads
+ * Image name is added to the database.
+ * Session ID is contained in req.body.session_id
+ * File name is contained in req.file.originalname
+ */
 module.exports.sendImage = function(req, res, next){
   debug("Image function called");
-  debug("req: " + req);
-  console.log(req);
-  var s_img = req.body.image;
-  var s_id = req.body.session_id;
-  debug("Image: " + s_img);
-  debug("Session ID: " + s_id);
+
+  var tmp_path = req.file.path;
+  var target_path = './uploads/' + req.file.originalname;
+  fs.rename(tmp_path, target_path, function(err) {
+      if (err) throw err;
+      fs.unlink(tmp_path, function() {
+          if (err) throw err;
+          // TODO: Function call to OCR module
+          debug('File uploaded to: ' + target_path + ' - ' + req.file.size + ' bytes');
+      });
+  });
+
+  var query = Bills.where({bill_id: req.body.session_id});
+  query.update({$set: {bill_image : req.file.originalname}}).exec();
 
   debug('Sending response (status: 200)');
   res.status(200).send("Success");
--- routes/mobile.js
+++ routes/mobile.js
@@ -1,14 +1,13 @@
 var debug     = require('debug')('platypus-api:routes:mobile');
 var express   = require('express');
 var mobile    = require('../controllers/mobile');
-var multer    = require('multer');
-var upload    = multer({ dest: '../uploads/' });
+
 
 var router = express.Router();
 
 router.post('/createSession', mobile.createSession);
 
-router.post('/sendImage', upload.single('bill'), mobile.sendImage);
+router.post('/sendImage', mobile.sendImage);
 
 debug('Mobile router exported');
 module.exports = router;
