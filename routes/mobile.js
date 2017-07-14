bc88e82e0281a33bdd118fd617831c9aff045e2e
Matthew Perry <matthew.perry.sa@gmail.com>
2017-07-14 13:54:22 +0200

    feat(LiveSession): terminateSession route
    added a route to the terminateSessionMethod()
    
    Signed-off-by: Matthew Perry <matthew.perry.sa@gmail.com>

--- routes/mobile.js
+++ routes/mobile.js
@@ -9,5 +9,7 @@ router.post('/createSession', mobile.createSession);
 
 router.post('/sendImage', mobile.sendImage);
 
+router.post('/terminateSession', mobile.terminateSession);
+
 debug('Mobile router exported');
 module.exports = router;
