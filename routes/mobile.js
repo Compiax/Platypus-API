1281c8af9c4a80c3f4cc37c515a75b3bf5f1cbdd
Matthew Perry <matthew.perry.sa@gmail.com>
2017-07-28 09:31:38 +0200

    feat(documentation): added JSDoc comments to routes/mobile.js
    Signed-off-by: Matthew Perry <matthew.perry.sa@gmail.com>

--- routes/mobile.js
+++ routes/mobile.js
@@ -1,14 +1,25 @@
+/**
+ * @file Defines the routes used by the mobile application for communiction
+ * with the API
+ */
 var debug     = require('debug')('platypus-api:routes:mobile');
 var express   = require('express');
 var mobile    = require('../controllers/mobile');
 
 
 var router = express.Router();
-
+/**
+ * Define route for the createSession function.
+ */
 router.post('/createSession', mobile.createSession);
 
+/**
+ * Define Route for the sendImage function.
+ */
 router.post('/sendImage', mobile.sendImage);
-
+/**
+ * Define route for the terminateSession function.
+ */
 router.post('/terminateSession', mobile.terminateSession);
 
 debug('Mobile router exported');
