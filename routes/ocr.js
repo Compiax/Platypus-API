ec81732b645b6eadf7f0fa33d2ff626b21d0e608
Matthew Perry <matthew.perry.sa@gmail.com>
2017-07-28 09:35:04 +0200

    feat(documentation): added JSDoc comments for routes/ocr.js
    Signed-off-by: Matthew Perry <matthew.perry.sa@gmail.com>

--- routes/ocr.js
+++ routes/ocr.js
@@ -1,9 +1,14 @@
+/**
+ * @file This defines the routes to be used by the OCR componant.
+ */
 var debug     = require('debug')('platypus-api:routes:ocr');
 var express   = require('express');
 var ocr     = require('../controllers/ocr');
 
 var router = express.Router();
-
+/**
+ * Define route for the CreateSession function.
+ */
 router.post('/createSession', ocr.createSession);
 
 debug('OCR router exported');
