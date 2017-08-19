9a5098e475e8eb4d9c697ae726052816bfb399b2
Wanrick <wanrick.pro@gmail.com>
2017-08-19 16:26:52 +0200

    feat(API): Session creator automatically joins session

--- models/bills.js
+++ models/bills.js
@@ -17,39 +17,4 @@ var Bills = new Schema({
   bill_items  : [{ type: ObjectId, ref: 'Items' }]
 });
 
-debug('Adding custom schema method: generateBillsID');
-/**
- * Method to generate and store bills ID.
- */
-Bills.methods.generateBillID = function() {
-  var alphabet = 'abcdefghjklmnopqrstvwxyz';
-  var numbers = '0123456789';
-  var bill_id_temp = "";
-
-  for (var i = 0; i < 5; i++) {
-    var char_or_int = (Math.floor(Math.random() * 2));
-
-    if (char_or_int === 0) {
-      var alphabet_index = (Math.floor(Math.random() * 24));
-      bill_id_temp = bill_id_temp + alphabet.charAt(alphabet_index);
-    }
-    else {
-      var numbers_index = (Math.floor(Math.random() * 10));
-      bill_id_temp = bill_id_temp + numbers.charAt(numbers_index);
-    }
-  }
-
-  this.model('Bills').find({ bill_id : bill_id_temp}).exec(function(err, res) {
-    if (res.length || err != null) {
-      debug('res = ' + res + ' and err = ' + err);
-      console.log('Error: Session ID exists!');
-      return false;
-    }
-  });
-  debug('Setting bill_id_temp = ' + bill_id_temp);
-  this.bill_id = bill_id_temp;
-  return true;
-};
-
-debug('Bills model exported');
 module.exports = mongoose.model('Bills', Bills);
