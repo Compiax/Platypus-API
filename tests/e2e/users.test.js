3821617dd66bff3950d7e192544be8bd295f4fc9
Matthew Perry <matthew.perry.sa@gmail.com>
2017-10-06 14:18:06 +0200

    feat:(testing) Implemented testing for mobile/joinSession
    Signed-off-by: Matthew Perry <matthew.perry.sa@gmail.com>

--- tests/e2e/users.test.js
+++ tests/e2e/users.test.js
@@ -27,3 +27,31 @@ module.exports.test = function(){
     });
   });
 };
+
+module.exports.test = function(){
+  describe('session joining =>', function() {
+    describe('POST /mobile/joinSession =>', function() {
+      var agent = request.agent(app);
+
+      it('Join session test', function(done) {
+        agent
+          .post('/mobile/joinSession')
+          .send({
+            session_id: 'xxxxx'
+            username: 'testuser',
+            color: 'RGB(255,255,255)'
+          })
+          .type('form')
+          .set('Accept', 'application/json')
+          .expect('Content-Type', /json/)
+          .expect(200)
+          .end(function(err, res){
+            if (err) return done(err);
+            done();
+          });
+
+          //assert.equals(response.data.attributes.session_id, "")
+      });
+    });
+  });
+};
