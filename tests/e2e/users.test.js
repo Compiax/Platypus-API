8143c32f2991ae6fed3898f88701d34fe20a722b
Matthew Perry <matthew.perry.sa@gmail.com>
2017-10-13 09:47:51 +0200

    feat(testing): fixing bugs in testing
    Signed-off-by: Matthew Perry <matthew.perry.sa@gmail.com>

--- tests/e2e/users.test.js
+++ tests/e2e/users.test.js
@@ -4,7 +4,7 @@ var request = require('supertest');
 module.exports.test = function(){
   describe('session creation =>', function() {
     describe('POST /mobile/createSession =>', function() {
-      var agent = request.agent(app);
+      var agent = request.agent(app.listen());
 
       it('Create session test', function(done) {
         agent
@@ -26,12 +26,62 @@ module.exports.test = function(){
       });
     });
   });
+
+  describe('session joining =>', function() {
+    describe('POST /mobile/joinSession =>', function() {
+      var agent = request.agent(app.listen());
+
+      it('Join session test', function(done) {
+        agent
+          .post('/mobile/joinSession')
+          .send({
+            session_id: 'xxxxx',
+            username: 'testuser',
+            color: 'RGB(255,255,255)'
+          })
+          .type('form')
+          .set('Accept', 'application/json')
+          .expect('Content-Type', /json/)
+          .expect(200)
+          .end(function(err, res){
+            if (err) return done(err);
+            done();
+          });
+          done();
+          //assert.equals(response.data.attributes.session_id, "")
+      });
+    });
+  });
+
+  describe('sesion terminating =>', function(){
+    describe('POST /mobile/terminateSession =>', function() {
+      var agent = request.agent(app.listen());
+
+      it('Terminate Session Test', function(done) {
+        agent
+          .post('/mobile/terminateSession')
+          .send({
+            session_id: 'xxxxx'
+          })
+          .type('form')
+          .set('Accept', 'application/json')
+          .expect('Content-Type', 'text/html; charset=utf-8')
+          .expect(200)
+          .end(function (err, res) {
+            if (err) return done(err);
+            done();
+          });
+          //assert.equals(response.data.attributes.session_id, "");
+      });
+    });
+  });
 };
 
+/*
 module.exports.test = function(){
   describe('session joining =>', function() {
     describe('POST /mobile/joinSession =>', function() {
-      var agent = request.agent(app);
+      var agent = request.agent(app.listen());
 
       it('Join session test', function(done) {
         agent
@@ -54,21 +104,22 @@ module.exports.test = function(){
     });
   });
 };
-
+*/
+/*
 module.exports.test = function() {
   describe('sesion terminating =>', function(){
     describe('POST /mobile/terminateSession =>', function() {
-      var agent = request.agent(app);
+      var agent = request.agent(app.listen());
 
       it('Terminate Session Test', function(done) {
         agent
-          .post('mobile/terminateSession')
+          .post('/mobile/terminateSession')
           .send({
             session_id: 'xxxxx'
           })
           .type('form')
           .set('Accept', 'application/json')
-          .expect('Content-Type', /json/)
+          .expect('Content-Type', 'text/html; charset=utf-8')
           .expect(200)
           .end(function (err, res) {
             if (err) return done(err);
@@ -79,3 +130,4 @@ module.exports.test = function() {
     });
   });
 };
+*/
